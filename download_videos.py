26a4b3cfe2f8decb9639e05e10e62eb1e6cad8de
jonathanlehner <lehnerj@student.ethz.ch>
2022-02-19 21:20:10 +0100

    latest

--- download_videos.py
+++ download_videos.py
@@ -1,8 +1,8 @@
 import urllib.request
 import requests
 
-url = "vid2cleantxt-online.jonathanlehner.com/api/"
-url = "http://localhost:3000/api/"
+url = "https://vid2cleantxt-online.jonathanlehner.com/api/"
+#url = "http://localhost:3000/api/"
 my_email = "jonathanslehner@gmail.com"
 x = requests.get(url+"get_videos"+"?email="+my_email)
 
